2202fdf3cf75d273e9661478dd15ae2aca9a0c77
Keren Duchan <keren.duchan@gmail.com>
2023-09-06 16:21:55 +0300

    fleshing out EmailPreview

--- src/cmps/EmailPreview.jsx
+++ src/cmps/EmailPreview.jsx
@@ -1,3 +1,19 @@
+import { Link } from 'react-router-dom'
+
 export function EmailPreview({ email }) {
-    return <div>emailPreview {email.id}</div>
+    function getStarImg() {
+        return 'imgs/' + (email.isStarred ? 'starred.svg' : 'unstarred.svg')
+    }
+
+    return (
+        <article className="email-preview">
+            <Link to={`/email/${email.id}`}>
+                <img
+                    className="email-star-img"
+                    src={getStarImg(email.isStarred)}
+                />
+                <span class="email-preview-subject">{email.subject}</span>
+            </Link>
+        </article>
+    )
 }
