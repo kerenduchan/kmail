1a958a84c844e4602595379b37a0af5f01f8e528
Keren Duchan <keren.duchan@gmail.com>
2023-09-09 14:32:13 +0300

    *.WIP-FEATURE-email-compose

--- src/pages/EmailIndex.jsx
+++ src/pages/EmailIndex.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { useParams, useLocation, useNavigate, Outlet } from 'react-router'
+import { useParams, useLocation, Outlet } from 'react-router'
 
 // components
 import { EmailFilter } from '../cmps/EmailFilter'
@@ -8,33 +8,19 @@ import { EmailSidebar } from '../cmps/EmailSidebar'
 
 // services
 import { emailService } from '../services/email.service'
-import { Modal } from '../cmps/Modal'
 
 export function EmailIndex() {
     const [emails, setEmails] = useState(null)
     const [filter, setFilter] = useState(emailService.getDefaultFilter())
-    const [showCompose, setShowCompose] = useState(false)
     const params = useParams()
     const location = useLocation()
-    const navigate = useNavigate()
 
     useEffect(() => {
         loadEmails()
     }, [filter])
 
-    useEffect(() => {
-        if (location.pathname == '/email/compose') {
-            setShowCompose(true)
-        }
-    }, [location])
-
     function onSetFilter(fieldsToUpdate) {
         setFilter((prevFilter) => ({ ...prevFilter, ...fieldsToUpdate }))
-    }
-
-    function onCloseCompose() {
-        setShowCompose(false)
-        navigate('/email')
     }
 
     async function onUpdateEmail(email) {
@@ -68,30 +54,24 @@ export function EmailIndex() {
 
     if (!emails) return <div>Loading..</div>
 
-    const inner = params.emailId ? (
-        <Outlet />
-    ) : (
-        <>
-            <EmailFilter filter={filter} onSetFilter={onSetFilter} />
-            <EmailList
-                emails={emails}
-                onUpdateEmail={onUpdateEmail}
-                onDeleteEmail={onDeleteEmail}
-            />
-        </>
-    )
+    const inner =
+        location.pathname == '/email/compose' || params.emailId ? (
+            <Outlet />
+        ) : (
+            <>
+                <EmailFilter filter={filter} onSetFilter={onSetFilter} />
+                <EmailList
+                    emails={emails}
+                    onUpdateEmail={onUpdateEmail}
+                    onDeleteEmail={onDeleteEmail}
+                />
+            </>
+        )
 
     return (
         <section className="email-index">
             <EmailSidebar />
             <section className="email-index-main">{inner}</section>
-
-            {/* Email Compose Modal */}
-            <Modal
-                show={showCompose}
-                onClose={onCloseCompose}
-                renderComponent={<Outlet />}
-            />
         </section>
     )
 }
