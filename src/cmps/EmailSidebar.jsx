33d65b600aca7e3ba9756c3de444190b9f9e9183
Keren Duchan <keren.duchan@gmail.com>
2023-09-13 16:13:02 +0300

    wip gmail-like styling

--- src/cmps/EmailSidebar.jsx
+++ src/cmps/EmailSidebar.jsx
@@ -3,7 +3,8 @@ import { getAllFolders } from '../util'
 export function EmailSidebar({ activeFolder, onFolderClick, onComposeClick }) {
     function getClassName(folder) {
         return (
-            'email-sidebar-folder' + (activeFolder == folder ? ' active' : '')
+            `email-sidebar-folder ${folder}` +
+            (activeFolder == folder ? ' active' : '')
         )
     }
 
@@ -23,7 +24,6 @@ export function EmailSidebar({ activeFolder, onFolderClick, onComposeClick }) {
                     return (
                         <a
                             key={folder.id}
-                            to={`/email/${folder.id}`}
                             className={getClassName(folder.id)}
                             onClick={() => onFolderClick(folder.id)}
                         >
