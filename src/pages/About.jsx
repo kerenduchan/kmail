bcf137e7e7b0efef920085b1a2a9826ae8540c49
Keren Duchan <keren.duchan@gmail.com>
2023-09-15 15:04:07 +0300

    about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,3 +1,12 @@
 export function About() {
-    return <h1>About</h1>
+    return (
+        <div class="about-page">
+            <header>
+                <h1>About Us</h1>
+            </header>
+            <article>
+                We are Kmail. Our goal is to replace gmail by the year 2025.
+            </article>
+        </div>
+    )
 }
