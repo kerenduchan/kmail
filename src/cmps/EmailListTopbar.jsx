f493cecc38836037bd8a8b821d7ce1231bf16e18
Keren Duchan <keren.duchan@gmail.com>
2023-09-29 11:06:21 +0300

    wip feature: email list topbar actions

--- src/cmps/EmailListTopbar.jsx
+++ src/cmps/EmailListTopbar.jsx
@@ -1,4 +1,5 @@
 import { MultiSelector } from './MultiSelector'
+import { SmallActionButton } from './SmallActionButton'
 
 export function EmailListTopbar({
     multiSelectorState,
@@ -6,10 +7,27 @@ export function EmailListTopbar({
 }) {
     return (
         <div className="email-list-topbar">
+            {/* Checkbox */}
             <MultiSelector
                 state={multiSelectorState}
                 onFilterChange={onMultiSelectorFilterChange}
             />
+            {/* Actions */}
+            <EmailListTopbarActions state={multiSelectorState} />
         </div>
     )
+}
+
+function EmailListTopbarActions({ state }) {
+    if (state == 'none') {
+        // TODO
+        return <div></div>
+    } else {
+        return (
+            <div className="email-list-topbar-actions">
+                <SmallActionButton type="delete" />
+                <SmallActionButton type="unread" />
+            </div>
+        )
+    }
 }
