17237be05c29fcd9dd6cbfc840052795c926ef4a
Keren Duchan <keren.duchan@gmail.com>
2023-09-30 15:13:59 +0300

    remove wrapper div from small action button

--- src/cmps/EmailLabels.jsx
+++ src/cmps/EmailLabels.jsx
@@ -4,9 +4,11 @@ export function EmailLabels({ labels, onCreateClick }) {
     return (
         <div className="email-labels">
             <div className="email-labels-title">Labels</div>
-            <div className="email-labels-create-btn-container">
-                <SmallActionButton type="create" onClick={onCreateClick} />
-            </div>
+            <SmallActionButton
+                type="create"
+                className="email-labels-create-btn-container"
+                onClick={onCreateClick}
+            />
             <div className="email-labels-list">
                 {labels.map((label) => (
                     <div key={label.id}>{label.name}</div>
