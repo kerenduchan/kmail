a2feb3fecf4b29de26651529f075bc50520356a4
Keren Duchan <keren.duchan@gmail.com>
2023-10-05 08:51:53 +0300

    wip feature: form validation for create/edit label

--- src/cmps/email-label/EmailLabelCreate.jsx
+++ src/cmps/email-label/EmailLabelCreate.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import { Dialog } from '../Dialog'
+import { Formik, Form, Field } from 'formik'
+import * as Yup from 'yup'
 
 // Dialog for creating an email label
 export function EmailLabelCreate({ label, onCloseClick, onSaveClick }) {
@@ -15,39 +17,68 @@ export function EmailLabelCreate({ label, onCloseClick, onSaveClick }) {
         setDraft((prev) => ({ ...prev, name: ev.target.value }))
     }
 
+    const validationSchema = Yup.object().shape({
+        labelName: Yup.string().required('Required'),
+    })
+
+    function Input(props) {
+        return (
+            <>
+                <label htmlFor={props.id}>{props.label}</label>
+                <input autoFocus {...props} />
+            </>
+        )
+    }
+
     return (
         <Dialog
             title={label ? 'Edit Label' : 'New Label'}
             onCloseClick={onCloseClick}
         >
-            <div className="email-label-create">
-                <form
-                    className="email-label-create-form"
-                    onSubmit={onFormSubmit}
-                >
-                    <div className="email-label-create-form-field">
-                        <label htmlFor="label-name">Label name:</label>
-                        <input
-                            autoFocus
-                            type="text"
-                            id="label-name"
-                            onChange={onNameChange}
-                            value={draft.name}
-                        />
-                    </div>
-                </form>
-                <div className="email-label-create-actions">
-                    <button className="weak-action-btn" onClick={onCloseClick}>
-                        Cancel
-                    </button>
-                    <button
-                        className="strong-action-btn"
-                        onClick={onFormSubmit}
-                    >
-                        {label ? 'Save' : 'Create'}
-                    </button>
-                </div>
-            </div>
+            <Formik
+                initialValues={{
+                    labelName: label ? label.name : '',
+                }}
+                validationSchema={validationSchema}
+                onSubmit={(values) => {
+                    // same shape as initial values
+                    console.log(values)
+                }}
+            >
+                {({ errors, touched }) => (
+                    <Form className="email-label-create-form">
+                        <div className="email-label-create-form-field">
+                            <Field
+                                as={Input}
+                                name="labelName"
+                                id="labelName"
+                                label="Label name:"
+                            />
+                            <div
+                                className={`email-label-create-form-field-errors ${
+                                    errors.labelName && touched.labelName
+                                        ? 'visible'
+                                        : ''
+                                }`}
+                            >
+                                {errors.labelName}
+                            </div>
+                        </div>
+                        <div className="email-label-create-form-actions">
+                            <button
+                                type="button"
+                                className="weak-action-btn"
+                                onClick={onCloseClick}
+                            >
+                                Cancel
+                            </button>
+                            <button className="strong-action-btn" type="submit">
+                                {label ? 'Save' : 'Create'}
+                            </button>
+                        </div>
+                    </Form>
+                )}
+            </Formik>
         </Dialog>
     )
 }
